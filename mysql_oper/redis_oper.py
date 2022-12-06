99f5121e4d5bd74186d242431bb397a22f61c8f1
admin <137962037@qq.com>
2022-12-06 14:41:23 +0800

    mysql and redis oper

--- mysql_oper/redis_oper.py
+++ mysql_oper/redis_oper.py
@@ -1,6 +1,6 @@
 import redis
 
-con_redis = redis.Redis(host='172.31.81.52', password='072620', port=6379, decode_responses=True)
+con_redis = redis.Redis(host='172.31.95.244', password='072620', port=6379, decode_responses=True)
 
 # string格式
 # 单值塞数据查数据
@@ -42,3 +42,48 @@ print(con_redis.hdel('hash1', 'a', 'b'))
 print(con_redis.hgetall('hash1'))
 
 print('==' * 20)
+
+# list类型
+# list中添加元素，每个元素都添加到list的最左边
+con_redis.lpush('list1',1,2,3)
+# 相对应的，元素添加到list的最右边
+con_redis.rpush('list1',-1,-2,-3)
+# 获取list中元素
+# 根据索引切片获取
+print(con_redis.lrange('list1',0,100))
+# 根据索引单值获取
+print(con_redis.lindex('list1',0))
+# 从左边删除第一个元素
+con_redis.lpop('list1')
+# 从右边删除第一个元素
+con_redis.rpop('list1')
+print(con_redis.lrange('list1',0,-1))
+# list中删除指定的值，并指定删除个数,如下，0表示删除所有
+con_redis.lrem('list1',0,2)
+con_redis.lrem('list1',0,1)
+con_redis.lrem('list1',0,-1)
+con_redis.lrem('list1',0,-2)
+con_redis.lrem('list1',0,-3)
+print(con_redis.lrange('list1',0,-1))
+# 重置某个索引上的值,如下，修改左边第一位为100
+con_redis.lset('list1',0,100)
+print(con_redis.lrange('list1',0,-1))
+
+con_redis.linsert('list1','before',1,1000)
+print(con_redis.lrange('list1',0,-1))
+
+
+print('=='*20)
+
+# set集合操作
+con_redis.sadd('set1',1,2,3)
+# 获取集合中元素个数
+print(con_redis.scard('set1'))
+# 判断某个值是否是集合中元素
+print(con_redis.sismember('set1',1))
+print(con_redis.sismember('set1',-1))
+# 查看集合中所有元素
+print(con_redis.smembers('set1'))
+# 删除集合中某些值
+con_redis.srem('set1',1)
+print(con_redis.smembers('set1'))
